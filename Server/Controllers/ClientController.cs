fe6fc9e211fafb5c9dd28de0953b77f3ad06493f
ROG <luan.roger.2003@gmail.com>
2024-04-06 18:08:36 -0300

    feat: create controllers and use cases

---
+++ Server/Controllers/ClientController.cs
@@ -0,0 +1,62 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Server.Controllers.Exceptions;
+using Server.Controllers.Interfaces;
+using Server.Mappers;
+using Server.Models.Requests;
+using Server.Models.Responses;
+using Server.UseCases.Client;
+using Server.UseCases.Intefaces;
+using Server.Utils;
+
+namespace Server.Controllers;
+
+public class ClientController : IClientController
+{
+    private readonly IRequest<IEnumerable<ClientDto>, GetAllClientsQuery> _getAllClients;
+    private readonly IRequest<ClientDto, CreateNewClientCommand> _createNewClient;
+    
+    private readonly ClientResponseMapper _clientResponseMapper;
+    private readonly IValidator<CreateNewClientRequest> _createNewClientValidator;
+
+    public ClientController(IRequest<IEnumerable<ClientDto>, GetAllClientsQuery> getAllClients,
+        IRequest<ClientDto, CreateNewClientCommand> createNewClient,
+        ClientResponseMapper clientResponseMapper,
+        IValidator<CreateNewClientRequest> createNewClientValidator)
+    {
+        _getAllClients = getAllClients;
+        _createNewClient = createNewClient;
+        _clientResponseMapper = clientResponseMapper;
+        _createNewClientValidator = createNewClientValidator;
+    }
+    
+    public async Task<IEnumerable<ClientResponse>> GetAllClientsWithPagination(GetAllClientsWithPaginationRequest request)
+    {
+        (int page, int pageSize) = request;
+        GetAllClientsQuery query = new(page, pageSize);
+        
+        var clients = await _getAllClients.Handle(query);
+        var response = clients
+            .Select(_clientResponseMapper.MapClientDtoToClientRequest);
+
+        return response;
+    }
+
+    public async Task<ClientResponse> CreateNewClient(CreateNewClientRequest request)
+    {
+        ValidationResult validationResult = await _createNewClientValidator.ValidateAsync(request);
+        if(!validationResult.IsValid)
+        {
+            throw new InvalidRequestException(typeof(CreateNewClientRequest), 
+                validationResult.Errors.FormatToString());
+        }
+        
+        (string name, string email) = request;
+        CreateNewClientCommand command = new(name, email);
+
+        ClientDto clientDto = await _createNewClient.Handle(command);
+        
+        ClientResponse response = _clientResponseMapper.MapClientDtoToClientRequest(clientDto);
+        return response;
+    }
+}
---
+++ Server/UseCases/Client/CreateNewClient.cs
@@ -0,0 +1,34 @@
+﻿using Server.Mappers;
+using Server.Repositories;
+using Server.UseCases.Intefaces;
+
+namespace Server.UseCases.Client;
+
+public record CreateNewClientCommand(string name, string email);
+
+public class CreateNewClient : IRequest<ClientDto, CreateNewClientCommand>
+{
+    private readonly IClientRepository _clientRepository;
+    private readonly ClientMapper _clientMapper;
+    private readonly ClientDtoMapper _clientDtoMapper;
+
+    public CreateNewClient(IClientRepository clientRepository,
+        ClientMapper clientMapper,
+        ClientDtoMapper clientDtoMapper)
+    {
+        _clientRepository = clientRepository;
+        _clientMapper = clientMapper;
+        _clientDtoMapper = clientDtoMapper;
+    }
+    
+    public async Task<ClientDto> Handle(CreateNewClientCommand request)
+    {
+        Models.Client client = _clientMapper.MapCreateNewClientCommandToClient(request);
+
+        client = await _clientRepository.CreateNewClient(client);
+        await _clientRepository.FlushChanges();
+
+        ClientDto result = _clientDtoMapper.MapClientToClientDto(client);
+        return result;
+    }
+}
